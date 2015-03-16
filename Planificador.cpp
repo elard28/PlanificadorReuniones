fad77226353a8110d4c98011d13b3db5d16902fb
Jonathan <jonathanz4@yahoo.es>
2015-03-16 16:18:20 -0500

    creando la funcion de carga de archivo

--- Planificador.cpp
+++ Planificador.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
+
+vector<string> archives;
+
+void load(char* filename){
+    ifstream ar(filename);
+    char* buff[200];
+    string cad;
+    while (ar>>buff){
+	cad = cad + buff;
+    }
+    cout << cad << endl; 
+}
 
 int main()
 {
