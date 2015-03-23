2ce576f186367c861c50215c7416ce4aaac46487
jozeya <jonathanz4@yahoo.es>
2015-03-22 19:13:15 -0500

    actualizando load de archivos

--- Planificador.cpp
+++ Planificador.cpp
@@ -1,23 +1,86 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <ctype.h>
+#include <map>
 
 using namespace std;
 
-vector<string> archives;
+typedef std::vector<string> type_time;
+typedef map<string, type_time > type_calendar;
 
-void load(char* filename){
+map<string,type_calendar > users;
+
+void load(char* filename, char* username){
     ifstream ar(filename);
-    char* buff[200];
-    string cad;
-    while (ar>>buff){
-	cad = cad + buff;
+    type_calendar calendar;
+    string day, r_time;
+    type_time times;
+    char buff[200];
+
+    ar >> buff;
+    day = buff;
+    
+    while (ar >> buff){
+		if (isalpha(buff[0])){
+			calendar.insert( std::pair<string,type_time>(day,times) );
+			times.clear();
+			day = buff;
+		}else{
+			r_time = buff;
+			times.push_back(r_time);
+		}
     }
-    cout << cad << endl; 
+    calendar.insert( std::pair<string,type_time>(day, times) );
+    users.insert( std::pair<string,type_calendar>(username, calendar) );
 }
+
+void print(vector<string> tmp_time){
+	std::vector<string>::iterator it = tmp_time.begin();
+ 	for (; it!=tmp_time.end(); ++it)
+    	std::cout << "   " << *it << " ";	
+}
+
+void print(map<string, type_time > tmp_day){
+	std::map<string,type_time>::iterator it = tmp_day.begin();
+	std::cout << "time contains:\n";
+ 	for (; it!=tmp_day.end(); ++it){
+    	std::cout << "   " << it->first << " => ";
+    	print(it->second);
+    	cout << endl;	
+    }
+}
+
+void print(){
+	std::map<string,type_calendar>::iterator it = users.begin();
+	std::cout << "mymap contains:\n";
+ 	for (it = users.begin(); it!=users.end(); ++it){
+    	std::cout << it->first << " => " << '\n';
+    	print(it->second);
+ 	}
+} 
 
 int main()
 {
+	load("a.txt", "a");
+	load("b.txt", "b");
+	print();
 
 	return 0;
 }
+/*
+	char ch;
+ 
+   printf("Ingrese un caracter: ");
+ ch = getchar();
+ 
+ if (isdigit(ch))
+   printf("Es un numero");
+ else if (isalpha(ch)) {
+   if (isupper(ch))
+     printf("Es una letra mayuscula");
+   else
+     printf("Es una letra minuscula");
+ } else
+   printf("Es un caracter");*/
